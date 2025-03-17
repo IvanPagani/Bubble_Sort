83a783197512e3e5543f5c4f5a4aafaa7f3667ee
IvanPagani <ivanpagani07@gmail.com>
2025-03-17 13:34:43 -0300

    Optimized the code to ignore items that are already sorted (at the end of the array)

--- ruby.rb
+++ ruby.rb
@@ -1,24 +1,27 @@
 def bubble_sort(inputArray)
 
-    size = (inputArray.length)
+    size = inputArray.length
     exitLoop = false
 
     until exitLoop do
         # Exit the loop
         exitLoop = true
 
-        (size - 1).times do |i|
+        (size-1).times do |i|
             if inputArray[i] > inputArray[i+1]
+
                 temp = inputArray[i]
                 inputArray[i] = inputArray[i+1]
                 inputArray[i+1] = temp
-                
-                # Do not exit the loop if the array isn't fully sorted
+
                 exitLoop = false
             end
         end
+        p inputArray
+        # Ignore items swapped to the end of the array
+        size -= 1
     end
     return inputArray
 end
 
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([6,5,3,1,8,7,2,4])
